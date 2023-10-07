a8e4fbac55b18b4246643ef006717e5382358dac
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-10-07 10:58:30 +0200

    Fixing build.zig.

--- build.zig
+++ build.zig
@@ -1,30 +1,25 @@
 const std = @import("std");
-const Build = std.build;
 
-pub fn build(b: *Build) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "unfil",
-        .optimize = optimize,
         .target = target,
+        .optimize = optimize,
     });
+
     exe.addCSourceFiles(&.{
         "src/unfil.c",
     }, &.{
         "-std=c17",
-        "-Wpedantic",
         "-Wall",
         "-Wextra",
+        "-Wpedantic",
         "-Wshadow",
     });
 
-    exe.addIncludePath("src");
-
     exe.linkSystemLibrary("c");
     b.installArtifact(exe);
-
-    const run = b.addRunArtifact(exe);
-    run.step.dependOn(b.getInstallStep());
 }
